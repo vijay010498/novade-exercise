2127c62fc2d792281bd3d767c1925eb9734c939b
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2021-05-18 22:54:45 +0530

    changed_db_conn_properties

--- src/config/postgresConnection.ts
+++ src/config/postgresConnection.ts
@@ -1,10 +1,17 @@
 import { Pool as post } from "pg";
 
-const pool = new post({
+/*const pool = new post({
   user: "postgres",
   password: "vijay",
   database: "novade",
   host: "localhost",
+  port: 5432,
+});*/
+const pool = new post({
+  user: "gmhvvwnssgumrp",
+  password: "0f34e7ca297fc3ff3b1cf7ead7394cd52039eecf68b63694274ab5bd0c57e000",
+  database: "dc5u74b93hrtd8",
+  host: "ec2-34-193-113-223.compute-1.amazonaws.com",
   port: 5432,
 });
 
