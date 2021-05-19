acc88ed9920e01f02e0cbfcfaeb92f373f924ea7
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2021-05-19 15:28:28 +0530

    added_readme_file

--- src/config/postgresConnection.ts
+++ src/config/postgresConnection.ts
@@ -2,15 +2,15 @@
 This files the postgres configuration settings to connect to a postgres database. Make sure to create a database and the notes table using the queries mentioned in the database.sql file
  */
 import { Pool as post } from "pg";
-
+/*
 const pool = new post({
   user: "postgres",
   password: "vijay",
   database: "novade",
   host: "localhost",
   port: 5432,
-});
-/*const pool = new post({
+});*/
+const pool = new post({
   user: "gmhvvwnssgumrp",
   password: "0f34e7ca297fc3ff3b1cf7ead7394cd52039eecf68b63694274ab5bd0c57e000",
   database: "dc5u74b93hrtd8",
@@ -19,6 +19,6 @@ const pool = new post({
   ssl: {
     rejectUnauthorized: false,
   },
-});*/
+});
 
 export { pool as postgres }; //exporting the connection object to use in the routes endpoints
